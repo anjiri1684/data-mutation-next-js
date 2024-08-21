b34f195ca2888626beda714adf6037f51991319e
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-21 09:48:56 +0300

    set up the coding environment ready to start the project

---
+++ components/header.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import logo from "../assets/logo.png";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <Link href="/">
+        <Image src={logo} alt="Mobile phone with posts feed on it" priority />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/feed">Feed</Link>
+          </li>
+          <li>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
